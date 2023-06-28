2d6899587dd994736fab654563fb5c10ca7fee80
Arnaud Morin <arnaud.morin@ovhcloud.com>
2023-06-28 14:37:07 +0200

    Run with last docker
    Signed-off-by: Arnaud Morin <arnaud.morin@ovhcloud.com>

--- docker_yaqluator.sh
+++ docker_yaqluator.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ -f .env ]; then
-    source ./.env
+    source .env
 fi
 
 echo Terminating current/old Docker processes...
-docker-compose rm -fs
+docker compose rm -fs
 
 echo Building Docker containers and launching YAQLuator...
-docker-compose up --build -d
+docker compose up --build -d
 
 echo Open web browser to http://localhost:${YAQLUATOR_HTTP_PORT:-80}/...
